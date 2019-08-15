4314679e1fc5e180fb52bdb0d791e25c8baa3847
Murtaza Gulamali <mygulamali@gmail.com>
2019-08-15 17:37:00 +0100

    Disable strict warnings in RJSX mode in Emacs

--- _emacs.d/config/modes/rjsx-mode.el
+++ _emacs.d/config/modes/rjsx-mode.el
@@ -3,4 +3,5 @@
 
 (setq js-indent-level 2)
 (setq js-switch-indent-offset 2)
-;;(setq js2-strict-trailing-comma-warning nil)
+;; (setq js2-mode-show-parse-errors nil)
+(setq js2-mode-show-strict-warnings nil)
