7af97701f2046f1b7d191d955532b8d8dbadf3ab
Murtaza Gulamali <murtaza@gulamali.net>
2023-10-19 11:03:25 +0100

    Update installation script

--- install.sh
+++ install.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
 
 ENTRIES=$(ls -1d _*)
-DEFAULTS_DIR="${PWD}/defaults"
+ORIGINALS_DIR="${PWD}/originals"
 
 for ENTRY in $ENTRIES; do
-    DOTFILE="${HOME}/.${ENTRY:1}"
+  DOTFILE="${HOME}/.${ENTRY:1}"
 
-    # check if dotfile already exists
-    if [ -s "$DOTFILE" ]; then
-        if [ -L "$DOTFILE" ]; then
-            # dotfile exists as a symlink, remove it
-            rm "$DOTFILE"
-        else
-            # dotfile exists as a real file, move it to defaults directory
-            [[ ! -d "$DEFAULTS_DIR" ]] && mkdir -p "$DEFAULTS_DIR"
-            mv "$DOTFILE" "${DEFAULTS_DIR}/_${ENTRY:1}"
-        fi
+  # check if dotfile already exists
+  if [ -s "$DOTFILE" ]; then
+    if [ -L "$DOTFILE" ]; then
+      # dotfile exists as a symlink, remove it
+      rm "$DOTFILE"
+    else
+      # dotfile exists as a real file, move it to defaults directory
+      ORIGINAL_FILE="${ORIGINALS_DIR}/_${ENTRY:1}"
+      [[ ! -d "$ORIGINALS_DIR" ]] && mkdir -p "$ORIGINALS_DIR"
+      cmp --silent -- "$DOTFILE" "$ORIGINAL_FILE" || mv "$DOTFILE" "$ORIGINAL_FILE"
     fi
+  fi
 
-    ln -s "${PWD}/${ENTRY}" "$DOTFILE"
+  ln -s "${PWD}/${ENTRY}" "$DOTFILE"
 done
